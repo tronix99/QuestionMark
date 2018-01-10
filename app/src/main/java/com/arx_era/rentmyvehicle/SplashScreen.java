0fedeee1c088eaa5fba5bf797afdb14cb82fa9fa
Tronix99 <rajdevkar.6@gmail.com>
2018-01-10 20:10:46 +0530

    Initial commit

---
+++ app/src/main/java/com/arx_era/rentmyvehicle/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.arx_era.rentmyvehicle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by Tronix99 on 08-01-2018.
+ */
+
+public class SplashScreen extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
