6cb98cd83e9b50f92ffbb767c6ad78922edfc6be
Tronix99 <rajdevkar.6@gmail.com>
2018-01-11 21:31:35 +0530

    Loop removed (back button)

--- app/src/main/java/com/arx_era/rentmyvehicle/MainActivity.java
+++ app/src/main/java/com/arx_era/rentmyvehicle/MainActivity.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthListener;
     GoogleApiClient mGoogleApiClient;
+    TabLayout tabLayout;
 
     @Override
     protected void onStart() {
@@ -40,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         mAuth = FirebaseAuth.getInstance();
 
